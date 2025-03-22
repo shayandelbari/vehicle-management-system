4329dd6990153d8feb6d40b86450f94568d5bff0
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:19:35 -0400

    Implement Airplane class

--- Vehicles/Airplane.cs
+++ Vehicles/Airplane.cs
@@ -1,6 +1,22 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
 
-public class Airplane
+namespace Vehicles;
+
+public class Airplane : Vehicle
 {
+    public double Altitude { get; set; }
 
+    public Airplane(string name, double price, double speed, double altitude)
+        : base(name, price, speed, VehicleConstants.VehicleTypes.Airplane)
+    {
+        Altitude = altitude;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Altitude: {Altitude} meters");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.AirplaneTaxRate;
 }
