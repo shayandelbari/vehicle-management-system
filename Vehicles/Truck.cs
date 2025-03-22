622b04eaa416c8ff89e84c135157b8f14245f253
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:22:39 -0400

    Add Truck class

--- Vehicles/Truck.cs
+++ Vehicles/Truck.cs
@@ -1,6 +1,32 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class Truck
+namespace Vehicles;
+
+public class Truck : Vehicle
 {
+    private double loadCapacity;
+    public double LoadCapacity
+    {
+        get { return loadCapacity; }
+        set
+        {
+            if (value < 0) throw new InvalidCargoCapacityException("Load capacity cannot be negative!");
+            loadCapacity = value;
+        }
+    }
 
+    public Truck(string name, double price, double speed, double loadCapacity)
+        : base(name, price, speed, VehicleConstants.VehicleTypes.Truck)
+    {
+        LoadCapacity = loadCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Load Capacity: {LoadCapacity} kg");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.TruckTaxRate;
 }
