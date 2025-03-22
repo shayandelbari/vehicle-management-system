f132390ee54c1c776f89c982837fe876fa5f7a55
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:22:16 -0400

    Add RaceCar class

--- Vehicles/RaceCar.cs
+++ Vehicles/RaceCar.cs
@@ -1,6 +1,23 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class RaceCar
+namespace Vehicles;
+
+public class RaceCar : Car
 {
+    public bool TurboBoost { get; set; }
 
+    public RaceCar(string name, double price, double speed, string model, int horsepower, bool turboBoost)
+        : base(name, price, speed, model, horsepower)
+    {
+        TurboBoost = turboBoost;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Turbo Boost: {(TurboBoost ? "Yes" : "No")}");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.CarTaxRate;
 }
