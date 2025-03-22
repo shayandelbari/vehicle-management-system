3cc133221d75356b128ad9f08a2b098ff5f94d7b
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:22:29 -0400

    Add Train class

--- Vehicles/Train.cs
+++ Vehicles/Train.cs
@@ -1,6 +1,32 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class Train
+namespace Vehicles;
+
+public class Train : Vehicle
 {
+    private int units;
+    public int Units
+    {
+        get { return units; }
+        set
+        {
+            if (value < 0) throw new InvalidUnitsException("Units cannot be negative!");
+            units = value;
+        }
+    }
 
+    public Train(string name, double price, double speed, int units)
+        : base(name, price, speed, VehicleConstants.VehicleTypes.Train)
+    {
+        Units = units;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Units: {Units}");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.TruckTaxRate;
 }
