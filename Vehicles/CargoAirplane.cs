587e2a60ffe2d56b698f36748e8b4c8d5747690a
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:21:50 -0400

    Add CargoAirplane class

--- Vehicles/CargoAirplane.cs
+++ Vehicles/CargoAirplane.cs
@@ -1,6 +1,32 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class CargoAirplane
+namespace Vehicles;
+
+public class CargoAirplane : Airplane
 {
+    private double cargoCapacity;
+    public double CargoCapacity
+    {
+        get { return cargoCapacity; }
+        set
+        {
+            if (value < 0) throw new InvalidCargoCapacityException("Cargo capacity cannot be negative!");
+            cargoCapacity = value;
+        }
+    }
 
+    public CargoAirplane(string name, double price, double speed, double altitude, double cargoCapacity)
+        : base(name, price, speed, altitude)
+    {
+        CargoCapacity = cargoCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Cargo Capacity: {CargoCapacity} kg");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.AirplaneTaxRate;
 }
