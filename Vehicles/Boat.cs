9438063071e7660d4b3bf2767a09577a035c2e55
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:21:38 -0400

    Add Boat class

--- Vehicles/Boat.cs
+++ Vehicles/Boat.cs
@@ -1,6 +1,32 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class Boat
+namespace Vehicles;
+
+public class Boat : Vehicle
 {
+    private int seatingCapacity;
+    public int SeatingCapacity
+    {
+        get { return seatingCapacity; }
+        set
+        {
+            if (value < 0) throw new InvalidSeatingCapacityException("Seating capacity cannot be negative!");
+            seatingCapacity = value;
+        }
+    }
 
+    public Boat(string name, double price, double speed, int seatingCapacity)
+        : base(name, price, speed, VehicleConstants.VehicleTypes.Boat)
+    {
+        SeatingCapacity = seatingCapacity;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Seating Capacity: {SeatingCapacity}");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.BoatTaxRate;
 }
