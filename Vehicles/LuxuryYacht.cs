55d089e0cc1410aa1b94341d3f8e2964f78d93e9
shayandelbari <shayan.delbari@protonmail.com>
2025-03-22 18:22:03 -0400

    Add LuxuryYacht class

--- Vehicles/LuxuryYacht.cs
+++ Vehicles/LuxuryYacht.cs
@@ -1,6 +1,27 @@
-﻿namespace vehicleManagementSystem;
+﻿using Constants;
+using Exceptions;
 
-public class LuxuryYacht
+namespace Vehicles;
+
+public class LuxuryYacht : Boat
 {
+    public bool HasHelipad { get; set; }
 
+    public LuxuryYacht(string name, double price, double speed, int seatingCapacity, bool hasHelipad)
+        : base(name, price, speed, seatingCapacity)
+    {
+        if (seatingCapacity < 0)
+        {
+            throw new InvalidSeatingCapacityException("Seating capacity cannot be negative.");
+        }
+        HasHelipad = hasHelipad;
+    }
+
+    public override void DisplayInfo()
+    {
+        base.DisplayInfo();
+        Console.WriteLine($"Helipad: {(HasHelipad ? "Yes" : "No")}");
+    }
+
+    public override double CalculateTax() => Price * VehicleConstants.TaxRates.BoatTaxRate;
 }
