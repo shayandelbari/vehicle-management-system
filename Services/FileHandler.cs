7a419a1e396aef3607f5d60c08243c41e06ff665
shayandelbari <shayan.delbari@protonmail.com>
2025-03-27 17:18:01 -0400

    Implement vehicle file handling and management methods

--- Services/FileHandler.cs
+++ Services/FileHandler.cs
@@ -1,6 +1,64 @@
-﻿namespace vehicleManagementSystem;
+﻿using Vehicles;
+using Constants;
 
-public class FileHandler
+namespace vehicleManagementSystem;
+
+public static class FileHandler
 {
+    private static readonly string filePath = "vehicles.csv";
 
+    public static void SaveVehicles(Vehicle[] vehicles)
+    {
+        using StreamWriter writer = new(filePath);
+        foreach (Vehicle vehicle in vehicles) writer.WriteLine(vehicle.ToString());
+    }
+
+    public static Vehicle[] LoadVehicles()
+    {
+        if (!File.Exists(filePath))
+        {
+            return [];
+        }
+
+        List<Vehicle> vehicles = [];
+        using StreamReader reader = new(filePath);
+        while (!reader.EndOfStream)
+        {
+            string? dataLine = reader.ReadLine();
+            if (string.IsNullOrWhiteSpace(dataLine)) continue;
+
+            string[] data = dataLine.Split(',');
+            VehicleConstants.VehicleTypes vehicleType = Enum.Parse<VehicleConstants.VehicleTypes>(data[0]);
+
+            switch (vehicleType)
+            {
+                case VehicleConstants.VehicleTypes.Car:
+                    vehicles.Add(new Car(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), data[4], Convert.ToInt32(data[5])));
+                    break;
+                case VehicleConstants.VehicleTypes.RaceCar:
+                    vehicles.Add(new RaceCar(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), data[4], Convert.ToInt32(data[5]), Convert.ToBoolean(data[6])));
+                    break;
+                case VehicleConstants.VehicleTypes.Truck:
+                    vehicles.Add(new Truck(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToDouble(data[4])));
+                    break;
+                case VehicleConstants.VehicleTypes.Train:
+                    vehicles.Add(new Train(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToInt32(data[4])));
+                    break;
+                case VehicleConstants.VehicleTypes.Boat:
+                    vehicles.Add(new Boat(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToInt32(data[4])));
+                    break;
+                case VehicleConstants.VehicleTypes.LuxuryYacht:
+                    vehicles.Add(new LuxuryYacht(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToInt32(data[4]), Convert.ToBoolean(data[5])));
+                    break;
+                case VehicleConstants.VehicleTypes.Airplane:
+                    vehicles.Add(new Airplane(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToDouble(data[4])));
+                    break;
+                case VehicleConstants.VehicleTypes.CargoAirplane:
+                    vehicles.Add(new CargoAirplane(data[1], Convert.ToDouble(data[2]), Convert.ToDouble(data[3]), Convert.ToDouble(data[4]), Convert.ToDouble(data[5])));
+                    break;
+            }
+        }
+
+        return [.. vehicles];
+    }
 }
--- Services/VehicleManager.cs
+++ Services/VehicleManager.cs
@@ -1,6 +1,33 @@
-﻿namespace vehicleManagementSystem;
+﻿using Vehicles;
+
+namespace vehicleManagementSystem;
 
 public class VehicleManager
 {
+    private List<Vehicle> vehicles = [];
 
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        vehicles.Remove(vehicle);
+    }
+
+    public Vehicle[] GetAllVehicles()
+    {
+        return [.. vehicles];
+    }
+
+    public void SaveVehicles()
+    {
+        FileHandler.SaveVehicles([.. vehicles]);
+    }
+
+    public void LoadVehicles()
+    {
+        vehicles = [.. FileHandler.LoadVehicles()];
+    }
 }
