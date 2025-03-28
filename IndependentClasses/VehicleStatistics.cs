2ce4c1270b790a61aa35004172a39a69d927faa9
shayandelbari <shayan.delbari@protonmail.com>
2025-03-28 14:52:35 -0400

    Added VehicleStatistics

--- IndependentClasses/VehicleStatistics.cs
+++ IndependentClasses/VehicleStatistics.cs
@@ -1,6 +1,23 @@
-﻿namespace vehicleManagementSystem;
+﻿using Vehicles;
+using Constants;
 
-public class VehicleStatistics
+namespace vehicleManagementSystem;
+
+public static class VehicleStatistics
 {
+    public static double AveragePrice(Vehicle[] vehicles)
+    {
+        return vehicles.Average(v => v.Price);
+    }
 
+    public static Vehicle FastestVehicle(Vehicle[] vehicles)
+    {
+        VehicleComparer.SortBySpeed(vehicles);
+        return vehicles[^1];
+    }
+
+    public static int CountByType(Vehicle[] vehicles, VehicleConstants.VehicleTypes type)
+    {
+        return vehicles.Count(v => v.VehicleType == type);
+    }
 }
